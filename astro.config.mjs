4f58196fe33ef3b331882b487119510ed3ff439d
Mitchell Salomon <mitchell_salomon@brown.edu>
2024-03-06 11:57:44 -0500

    feat: action to deploy to github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://Mitchman215/github.io',
+  base: '/cs1300-portfolio',
+});
